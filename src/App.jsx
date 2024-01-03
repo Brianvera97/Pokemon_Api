74ecaba450a7664389300f49538909c2f0385b64
Brianvera97 <Bryanverac@gmail.com>
2024-01-03 13:01:45 -0300

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
   const [pokemonList, setPokemonList] = useState([]);
 
-  const handleClick = () => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        setPokemonList(data.results);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0");
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      setPokemonList(data.results);
+    }
+    catch (error) {
+      console.error('Error fetching Pokémon:', error);
+    }
   }
 
   return (
     <>
       <button type="button" onClick={handleClick}>Fetch Pokemon</button>
 
-      
+
       <ul>
         {pokemonList.map((pokemon, index) => (
           <li key={index}>{pokemon.name}</li>
